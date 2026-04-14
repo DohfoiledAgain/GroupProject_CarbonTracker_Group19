be009b3533a79bc815b478d5b90f8ac3de5be1a1
nicko <nickrev55@gmail.com>
2026-04-14 17:01:08 +0100

    updated styling of login prompt
    - updated styling of login prompt

--- Website/components/loginprompt.php
+++ Website/components/loginprompt.php
@@ -7,11 +7,12 @@
 
 <div id="login-overlay" style="<?php echo !$user ? 'display:flex' : 'display:none'; ?>">
   <div class="content">
-    <h1>You are not signed in</h1>
+    <h1 style="font-family: Glacial Indifference Bold">You are not signed in</h1>
     <form method="POST" style="display:flex;  flex-direction: column; align-items: center;">
       <input type="submit" class="sign-in-button" id="signin-redir" name="signin-redir" value="Sign In">
-      <input type="submit" class="sign-up-button" id="signup-redir" name="signup-redir" value="Sign Up"><p></p>
-      <input type="submit" class="about-us-button" id="about-us-redir" name="about-us-redir" value="About Us">
+      <br>
+      <input type="submit" class="sign-up-button" style="width: 311.6px" id="signup-redir" name="signup-redir" value="Sign Up"><p></p>
+      <input type="submit" class="about-us-button" style="font-style: italic;" id="about-us-redir" name="about-us-redir" value="About Us">
     </form>
   </div>
 </div>
