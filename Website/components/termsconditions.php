ef0e239b06f30ece8576ac78f7b40869d1c4d940
TobyP-VII <tobyp2404@gmail.com>
2026-04-27 17:00:49 +0100

    terms and conditions I guess

--- Website/components/termsconditions.php
+++ Website/components/termsconditions.php
@@ -1,7 +1,7 @@
 <div id="tc-overlay">
   <div class="content">
     <h1>Terms and Conditions</h1>
-    <div id="scroll-box">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</div>
+    <div id="scroll-box">we arent liable<br>take everything with a grain of salt <br>data will be secure probably</div>
     <div id="checkbox">
       <input type="checkbox" id="tc-accept-cb" name="Accept" value="True">
       <label for="tc-accept-cb"> I have read and accept the Terms</label><br>
